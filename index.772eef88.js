17f4b4b2715043f869f4073984305594d5d25187
val-mus <105125256+val-mus@users.noreply.github.com>
2022-11-06 19:40:06 +0000

    Deploying to gh-pages from @ val-mus/goit-js-hw-10@e55b299c81520ff62e13c387a91cc460753d22c8 🚀

---
+++ index.772eef88.js
@@ -0,0 +1,2 @@
+fetch(`https://restcountries.com/v3.1/name/${"uk"}`).then((e=>e.json())).then((e=>console.log(e)));
+//# sourceMappingURL=index.772eef88.js.map
---
+++ index.ead8c8e2.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/".concat("uk")).then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.ead8c8e2.js.map
