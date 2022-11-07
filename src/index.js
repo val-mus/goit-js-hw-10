7fb241510640573edc6a1ed965147c066651eaba
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-11-08 01:37:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Notiflix from 'notiflix';
 
 import fetchCountries from './fetchCountries';
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 const getEl = element => document.querySelector(element);
 
